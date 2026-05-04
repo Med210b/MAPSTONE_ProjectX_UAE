193aaa63c31c6b91456a3406c77f332677ec7d6b
MAPSTONE DUBAI <mohamedbentaher250@gmail.com>
2026-05-04 04:40:47 +0400

    Fix exact repo name in base path and homepage

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: "/MAPSTONEprojectX/",
+    base: "/MAPSTONE_ProjectX_UAE/",
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
